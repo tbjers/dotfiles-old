f7c5df85572e3beb4702af18577cbc3c2905a0b6
Torgny Bjers <torgny.bjers@gmail.com>
2021-09-23 14:58:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-set -e # -e: exit on error
+# exit on error
+set -e
+
+# authorise sudo early on
+if ! sudo -n echo 2>/dev/null; then
+    echo "Please enter sudo password. Sudo session will be kept alive until this script exits."
+    # sudo -v is technically correct but asks for a non-existent password on fresh AWS Ubuntu AMIs
+    sudo echo -n
+fi
+
+# sudo keepalive. This will keep the sudo watchdog fed until this script exits.
+# This works buy poking the parent process to see if it's still alive.
+while true; do sudo -n true; sleep 15; kill -0 "$$" || exit; done 2>/dev/null &
 
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/bin"
