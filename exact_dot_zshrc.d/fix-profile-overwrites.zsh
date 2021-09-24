99119f7955d5c1ed562ee4a7724feaa8cff8a880
Torgny Bjers <torgny.bjers@gmail.com>
2021-09-24 13:44:34 -0400

    update ssh/gpg agents

--- exact_dot_zshrc.d/fix-profile-overwrites.zsh
+++ exact_dot_zshrc.d/fix-profile-overwrites.zsh
@@ -1,4 +1,7 @@
 # Fedora (and others) stomp over some variables in /etc/zprofile and /etc/zshrc we set
 # earlier. fix those
 
+unset SSH_AGENT_PID
 export SSH_ASKPASS=$(systemctl --user show-environment | grep SSH_ASKPASS | cut -f 2 -d '=')
+export GPG_TTY=$(tty)
+gpg-connect-agent updatestartuptty /bye >/dev/null
