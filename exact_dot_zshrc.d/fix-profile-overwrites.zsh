8f018bf4a805d535772cfc1b7c3126eb26539bae
Torgny Bjers <torgny.bjers@gmail.com>
2021-09-24 14:11:27 -0400

    update configs for gpg

--- exact_dot_zshrc.d/fix-profile-overwrites.zsh
+++ exact_dot_zshrc.d/fix-profile-overwrites.zsh
@@ -3,5 +3,9 @@
 
 unset SSH_AGENT_PID
 export SSH_ASKPASS=$(systemctl --user show-environment | grep SSH_ASKPASS | cut -f 2 -d '=')
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
 export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
+
