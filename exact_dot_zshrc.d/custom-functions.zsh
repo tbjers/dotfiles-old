40cbdf7795efd02c2dd5609d014c7e467b212bfd
Torgny Bjers <torgny.bjers@gmail.com>
2021-10-04 23:00:33 -0600

    add initial mbsync/notmuch/afew/alot config

--- exact_dot_zshrc.d/custom-functions.zsh
+++ exact_dot_zshrc.d/custom-functions.zsh
@@ -1,4 +1,12 @@
 # -*- mode:sh; -*-
 function fn_exists() {
-        type $1 | grep -q 'shell function'
+    type $1 | grep -q 'shell function'
+}
+
+function op() {
+    OP_SESSION_bjers=$(onepassword-signin) command op --cache "$@"
+}
+
+function chezmoi() {
+    OP_SESSION_bjers=$(onepassword-signin) command chezmoi "$@"
 }
