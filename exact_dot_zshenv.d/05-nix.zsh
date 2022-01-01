f82a0d7b1ffe91d0c137d62992e44f744ed2fef1
Torgny Bjers <torgny@hey.com>
2022-01-01 07:17:44 -0500

    update nix installaer configs

--- exact_dot_zshenv.d/05-nix.zsh
+++ exact_dot_zshenv.d/05-nix.zsh
@@ -1,1 +1,5 @@
-if [ -e /home/tbjers/.nix-profile/etc/profile.d/nix.sh ]; then . /home/tbjers/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+elif [ -e '/home/tbjers/.nix-profile/etc/profile.d/nix.sh' ]; then
+  . '/home/tbjers/.nix-profile/etc/profile.d/nix.sh';
+fi
